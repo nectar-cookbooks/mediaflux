a37ca85b9e55015ef6b282d2c2a2000317677a68
Stephen Crawley <s.crawley@uq.edu.au>
2014-01-21 14:58:22 +1000

    Fixes for openstack credentials file

--- recipes/default.rb
+++ recipes/default.rb
@@ -330,6 +330,7 @@ end
 
 if backup_store != '' then
   node.normal['qcloud']['openstack_rc_path'] = '/etc/mediaflux/openstackrc'
+  node.normal['qcloud']['openstack_rc_user'] = mflux_user
 end
 
 template "#{mflux_home}/bin/backup.sh" do
