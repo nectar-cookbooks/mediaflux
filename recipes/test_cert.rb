85131e72cb691936600169c2bcf04eeb20b7254b
Stephen Crawley <s.crawley@uq.edu.au>
2014-02-03 14:04:48 +1000

    Recipe to generate a self-signed test cert

---
+++ recipes/test_cert.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: mediaflux
+# Recipe:: test_cert
+#
+# Copyright (c) 2014, The University of Queensland
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# * Neither the name of the The University of Queensland nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF QUEENSLAND BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+installers = node['mediaflux']['installers']
+keystore = "#{installers}/certs"
+password = "password"
+dname = "cn=#{node.hostname} ou=OU o=O l=L st=ST c=AU"  # dummy values ...
+
+if !File::exists?(keystore) then
+  bash "generate test keystore" do
+    code "keytool -selfcert -genkeypair -keystore #{keystore} -alias mflux " +
+         "-keyalg RSA -validity 365 -dname '#{dname}' " +
+         "-storepass #{password} -storepass #{password}"
+  end
+end
